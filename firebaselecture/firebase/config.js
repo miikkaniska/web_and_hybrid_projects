b25d9c3e91d4fcd03b0e5512fafce2ecb5ce7926
Miikka Niska <78212251+miikkaniska@users.noreply.github.com>
2023-11-07 22:43:56 +0200

    Update config.js

--- firebaselecture/firebase/config.js
+++ firebaselecture/firebase/config.js
@@ -2,12 +2,7 @@ import { initializeApp } from 'firebase/app'
 import { getFirestore, collection, addDoc, serverTimestamp,query, onSnapshot, orderBy} from 'firebase/firestore'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBgMAYUgQv7E5UDAH1hhH8GCfoVyz1ATI4",
-    authDomain: "lecturechat-a926d.firebaseapp.com",
-    projectId: "lecturechat-a926d",
-    storageBucket: "lecturechat-a926d.appspot.com",
-    messagingSenderId: "555626107544",
-    appId: "1:555626107544:web:04e53b28f18921139af6ed"
+// api keys and rest of the stuff, that cannot be uploeaded to github
   };
 
   initializeApp(firebaseConfig)
